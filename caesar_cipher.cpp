652945396b9c0274783859eeac4f256046b8442e
Th3N4m3 <144971731+TTN-ATTN@users.noreply.github.com>
2024-04-14 18:29:22 +0700

    Update caesar_cipher.cpp
    adding comments to the code

--- caesar_cipher.cpp
+++ caesar_cipher.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 class Cipher {
     private:
-        string str;
-        int sh;
+        string str; // ==== original string ==== //
+        int sh; // ==== shifting value ==== //
 
     public:
         Cipher() {};
         Cipher(string str, int sh) {
-            this->str = str;
-            this->sh = sh;
+            this->str = str; 
+            this->sh = sh; 
         };
 
+        // ==== read input ==== //
         friend istream& operator>>(istream& in, Cipher& c) {
             cout << "Enter your string: ";
             getline(in, c.str);
@@ -22,6 +23,7 @@ class Cipher {
             return in;
         }
 
+        // ==== caesar encryption ==== //
         void transform() {
             string res = "";
             for (auto ch : this->str) {
@@ -35,6 +37,7 @@ class Cipher {
             this->str = res;
         }
 
+        // ==== print ==== //
         friend ostream& operator<<(ostream& out, Cipher& c) {
             out << "Your encoded string is: ";
             out << c.str;
